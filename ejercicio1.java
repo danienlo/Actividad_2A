239ef2cbef9fcac68209a04fd2b645261bcbf615
danienlo <danny.sanchez46596@ucaldas.edu.co>
2023-09-27 22:22:02 -0500

    prueba

---
+++ ejercicio1.java
@@ -0,0 +1,11 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class ejercicio1{
+    static PrintStream scren = System.out; 
+    public static void main(String[] args) {
+        
+    }   
+}
+
+
